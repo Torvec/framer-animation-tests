72dbf3b369137ade820bcf363774e046721c0835
Edward Vonschondorf <edvonschondorf@gmail.com>
2024-03-09 10:29:21 -0800

    Setup page for animation tests

--- src/layout/Navigation.tsx
+++ src/layout/Navigation.tsx
@@ -1,16 +1,49 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function Navigation() {
+  function handleClickToLoc(loc: string | undefined) {
+    loc === "top"
+      ? window.scrollTo({ top: 0, behavior: "smooth" })
+      : loc === "bottom"
+      ? window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" })
+      : null;
+  }
+
   return (
-    <nav className="bg-neutral-700 px-10 py-2">
-      <ul className="flex gap-10">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="test">Test</Link>
-        </li>
-      </ul>
+    <nav className="fixed bottom-10 z-10 w-full uppercase text-lg">
+      <div className="mx-auto bg-neutral-700 w-max rounded-3xl bg-opacity-80">
+        <ul className="flex justify-evenly gap-2 p-4">
+          <li>
+            <Link to="/" className="bg-neutral-800 py-1.5 px-4 rounded-full">
+              Home
+            </Link>
+          </li>
+          <li>
+            <a
+              href=""
+              onClick={(e) => {
+                e.preventDefault();
+                handleClickToLoc("top");
+              }}
+              className="bg-neutral-800 py-1.5 px-4 rounded-full"
+            >
+              Top
+            </a>
+          </li>
+          <li>
+            <a
+              href=""
+              onClick={(e) => {
+                e.preventDefault();
+                handleClickToLoc("bottom");
+              }}
+              className="bg-neutral-800 py-1.5 px-4 rounded-full"
+            >
+              Bottom
+            </a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,11 +1,11 @@
-import { Outlet } from 'react-router-dom'
-import Navigation from './Navigation'
+import { Outlet } from "react-router-dom";
+import Navigation from "./Navigation";
 
 export default function RootLayout() {
   return (
-    <div className='h-screen bg-neutral-900 text-neutral-100'>
+    <div className="min-h-screen bg-neutral-900 text-neutral-100 relative overflow-hidden">
       <Navigation />
       <Outlet />
     </div>
-  )
+  );
 }
